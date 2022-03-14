539708b450a9a3be31d94d596e8b7354673d2095
Ryan-Matheus <ryanpqt266@gmail.com>
2022-03-14 17:04:37 -0300

    Moviment of Tinder cards

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by user on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
