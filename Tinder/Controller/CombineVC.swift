582e82b15bc79985f18cd90f6a297dadc108431c
Ryan-Matheus <ryanpqt266@gmail.com>
2022-03-16 10:14:20 -0300

    Card interation

--- Tinder/Controller/CombineVC.swift
+++ Tinder/Controller/CombineVC.swift
@@ -8,45 +8,108 @@
 import UIKit
 
 class CombineVC: UIViewController {
+    
+    var perfilButton: UIButton = .iconMenu(named: "icone-perfil")
+    var chatButton: UIButton = .iconMenu(named: "icone-chat")
+    var logoButton: UIButton = .iconMenu(named: "icone-logo")
+    
+    var deslikeButton: UIButton = .iconFooter(named: "icone-deslike")
+    var superlikeButton: UIButton = .iconFooter(named: "icone-superlike")
+    var likeButton: UIButton = .iconFooter(named: "icone-like")
+
+    
+    var usuarios: [Usuario] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.systemGroupedBackground
         
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = UIColor.systemGroupedBackground
+        self.adicionarFooter()
+        self.buscaUsuarios()
+        self.adicionarHeader()
+    }
+    
+    func buscaUsuarios () {
+        self.usuarios = UsuarioService.shared.buscaUsuarios()
         self.adicionarCards()
         
+      
     }
     
 }
 
 extension CombineVC {
+    func adicionarHeader () {
+        
+        let window = UIApplication.shared.windows.first { $0.isKeyWindow }
+        let top: CGFloat = window?.safeAreaInsets.top ?? 44
+        
+        let stackView = UIStackView(arrangedSubviews: [perfilButton, logoButton, chatButton])
+        stackView.distribution = .equalCentering
+        
+        view.addSubview(stackView)
+        stackView.preencher(
+            top: view.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: nil,
+            padding: .init(top: top, left: 16, bottom: 0, right: 16)
+            )
+    }
+    func adicionarFooter () {
+        let stackView = UIStackView(arrangedSubviews: [UIView(), deslikeButton, superlikeButton, likeButton, UIView()])
+        stackView.distribution = .equalCentering
+        
+        view.addSubview(stackView)
+        stackView.preencher(
+            top: nil,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: view.bottomAnchor,
+            padding: .init(top: 0, left: 16, bottom: 34, right: 16)
+        )
+    }
+}
+
+extension CombineVC {
     func adicionarCards () {
         
-        for item in 1...3 {
-        let redView = UIView()
-            redView.backgroundColor = item == 2 ? .blue : .red
-        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        for usuario in usuarios {
+        let card = CombineCardView()
+            card.frame = CGRect(x: 0, y: 0, width: view.bounds.width - 32, height: view.bounds.height * 0.7)
         
-        redView.center = view.center
+        card.center = view.center
+    
+            card.usuario = usuario
+            card.tag = usuario.id
         
         let gesture = UIPanGestureRecognizer()
         gesture.addTarget(self, action: #selector(handlerCard))
         
-        redView.addGestureRecognizer(gesture)
+        card.addGestureRecognizer(gesture)
         
-        view.addSubview(redView)
+            view.insertSubview(card, at: 0)
         }
     }
 }
 
 extension CombineVC {
     @objc func handlerCard (_ gesture: UIPanGestureRecognizer) {
-        if let card = gesture.view {
+        if let card = gesture.view as? CombineCardView {
             let point = gesture.translation(in: view)
             
             card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
             
             let rotantionAngle = point.x / view.bounds.width * 0.4
+            
+            if point.x > 0 {
+                card.likeImageView.alpha = rotantionAngle * 5
+                card.deslikeImageView.alpha = 0
+            } else {
+                card.likeImageView.alpha = 0
+                card.deslikeImageView.alpha = rotantionAngle * 5 * -1
+            }
             
             card.transform = CGAffineTransform(rotationAngle: rotantionAngle)
             
@@ -55,6 +118,9 @@ extension CombineVC {
                 UIView.animate(withDuration: 0.2) {
                 card.center = self.view.center
                     card.transform = .identity
+                    
+                    card.likeImageView.alpha = 0
+                    card.deslikeImageView.alpha = 0
                 }
             }
         }
